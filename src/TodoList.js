603554faefa301feb2e8b48a706f67f11e1045a0
codetophel <nicodemusakande@gmail.com>
2022-08-17 16:07:33 +0100

    modified:   package-lock.json 	modified:   package.json#	modified:   public/index.html
    	deleted:    public/logo512.png
    	modified:   src/App.css
    	modified:   src/App.js
    deleted:    src/App.test.js
    	new file:   src/NewTodoForm.js
    	new file:   src/Todo.css
    	new file:   src/Todo.js
    	new file:   src/TodoList.js
    	modified:   src/index.js
    	deleted:    src/logo.svg
    	deleted:    src/reportWebVitals.js
    	deleted:    src/setupTests.js

---
+++ src/TodoList.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { NewTodoForm } from './NewTodoForm';
+import Todo from './Todo';
+
+export class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+    };
+    this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
+    this.update = this.update.bind(this);
+    this.toggleCompletion = this.toggleCompletion.bind(this);
+  }
+
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  }
+
+  update(id, updatedTask) {
+    const updatedTodo = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, task: updatedTask };
+      }
+      return todo;
+    });
+    this.setState({ todos: updatedTodo });
+  }
+
+  remove(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id),
+    });
+  }
+
+  toggleCompletion(id) {
+    const updatedTodo = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    this.setState({ todos: updatedTodo });
+  }
+
+  render() {
+    const todos = this.state.todos.map((todo) => {
+      return (
+        <Todo
+          key={todo.id}
+          id={todo.id}
+          task={todo.task}
+          completed={todo.completed}
+          removeTodo={this.remove}
+          toggleTodo={this.toggleCompletion}
+          updateTodo={this.update}
+        />
+      );
+    });
+    return (
+      <div>
+        <h1> Todo List!</h1>
+        <NewTodoForm createTodo={this.create} />
+        <ul>{todos}</ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
